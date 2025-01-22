4086f43bb0a0f2473d070042c84cb24701a2173f
Michael Kainz <holznudelholz@gmail.com>
2025-01-22 23:32:38 +0100

    refactor module structure, add dotenv support, and update build process

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,10 @@
 import copy from "rollup-plugin-copy";
-import { defineConfig } from "vite";
+import { defineConfig, Plugin } from "vite";
+import * as fsPromises from "fs/promises";
+import dotenv from 'dotenv';
+import path from 'path';
+
+dotenv.config({ path: path.resolve(__dirname, '.env') });
 
 export default defineConfig({
   build: {
@@ -15,12 +20,52 @@ export default defineConfig({
   },
   plugins: [
     copy({
-      targets: [{ src: "./module.json", dest: "dist" },
-      { src: "./src/roll-window.html", dest: "dist" },
-      { src: "./src/lsm-style.css", dest: "dist" },
-      { src: "./src/tables", dest: "dist" }
+      targets: [
+        { src: "./src/roll-window.html", dest: "dist" },
+        { src: "./src/lsm-style.css", dest: "dist" },
+        { src: "./src/tables", dest: "dist" }
       ],
       hook: "writeBundle",
     }),
+    updateModuleManifestPlugin(),
   ],
 });
+
+function updateModuleManifestPlugin(): Plugin {
+  return {
+    name: "update-module-manifest",
+    async writeBundle(): Promise<void> {
+      console.log("Updating module manifest");
+      const moduleVersion = process.env.MODULE_VERSION;
+      const githubProject = process.env.GH_PROJECT;
+      const githubTag = process.env.GH_TAG;
+      const manifestContents: string = await fsPromises.readFile(
+        "src/module.json",
+        "utf-8"
+      );
+      const manifestJson = JSON.parse(manifestContents) as Record<
+        string,
+        unknown
+      >;
+      console.log(moduleVersion);
+      console.log(manifestJson);
+      if (moduleVersion) {
+        manifestJson["version"] = moduleVersion;
+      }
+      if (githubProject) {
+        const baseUrl = `https://github.com/${githubProject}/releases`;
+        manifestJson["manifest"] = `${baseUrl}/latest/download/module.json`;
+        if (githubTag) {
+          manifestJson[
+            "download"
+          ] = `${baseUrl}/download/${githubTag}/module.zip`;
+        }
+      }
+
+      await fsPromises.writeFile(
+        "dist/module.json",
+        JSON.stringify(manifestJson, null, 4)
+      );
+    },
+  };
+}
